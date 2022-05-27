060b90edbba1db2d463394c578bc9f831712bcfd
jack-pearson <qize953463876@gmail.com>
2022-05-28 00:27:05 +0800

    增加插件,login页面初始化

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,16 @@
+/*
+ * @Author: jack-pearson qize953463876@gmail.com
+ * @Date: 2022-05-27 14:27:11
+ * @LastEditors: jack-pearson qize953463876@gmail.com
+ * @LastEditTime: 2022-05-27 17:24:07
+ * @FilePath: \GOSS-APP\src\pages\login.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from "react";
-import { View } from "react-native";
+import { Text } from "react-native";
 
 const LoginPage = () => {
-  return <View>123</View>;
+  return <Text>123</Text>;
 };
 
 export default LoginPage;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-27 14:27:35
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-27 16:51:04
+ * @LastEditTime: 2022-05-27 17:55:30
  * @FilePath: \GOSS-APP\src\router\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,15 +10,36 @@ import React from "react";
 import "react-native-gesture-handler";
 import { connect } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack";
 import { setUser } from "@/actions/user";
 import Pages from "./importPage";
 const Stack = createStackNavigator();
 const Router = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='login' component={Pages.LoginPage} />
+      <Stack.Navigator
+        screenOptions={({ route }) => {
+          const info = route.name === "LoginScreen" ? TransitionPresets.ModalSlideFromBottomIOS : TransitionPresets.SlideFromRightIOS;
+          return {
+            headerStyle: {
+              elevation: 0,
+              borderBottomWidth: 0,
+            }, // 一个应用于 header 的最外层 View 的 样式对象
+            headerTintColor: "black",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            mode: "modal",
+            headerBackTitleVisible: false,
+            headerTitleAlign: "center",
+            cardStyle: {
+              flex: 1,
+              backgroundColor: "#f5f5f9",
+            },
+            ...info,
+          };
+        }}>
+        <Stack.Screen name='1' component={Pages.LoginPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
