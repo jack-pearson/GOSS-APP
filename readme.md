ae12ed1ef7ddf09b0bda6f5a7b282756fabf13c6
jack-pearson <qize953463876@gmail.com>
2022-05-30 14:48:44 +0800

    增加ui库. 图标库

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-26 14:10:56
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-28 00:28:36
+ * @LastEditTime: 2022-05-30 14:48:09
  * @FilePath: \GOSS-APP\readme.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -28,3 +28,7 @@ redux-thunk
 babel-plugin-root-import
 @react-native-community/async-storage
 react-native-gesture-handler
+jetifier // 不用这个会报错
+
+react-native-elements // UI 库
+react-native-vector-icons // 图标库
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,14 +2,16 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-27 14:27:11
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-29 15:12:43
+ * @LastEditTime: 2022-05-30 14:46:45
  * @FilePath: \GOSS-APP\src\pages\login.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { removeUser, setUser } from "@/actions/user";
 import React, { useEffect } from "react";
-import { Button, Text, View, TouchableOpacity, Alert } from "react-native";
+import { Text, View, TouchableOpacity, Alert } from "react-native";
 import { connect } from "react-redux";
+import { Button, Slider } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const LoginPage = ({ user, setUser, removeUser }) => {
   useEffect(() => {
@@ -27,7 +29,8 @@ const LoginPage = ({ user, setUser, removeUser }) => {
         }}>
         <Text>12312</Text>
       </TouchableOpacity>
-      <Button title='Press me' onPress={() => Alert.alert("Simple Button pressed")} />
+      <Button type='solid' icon={<Icon name='arrow-right' size={20} color='blue' />} title='Press me' onPress={() => Alert.alert("Simple Button pressed")} />
+      <Slider value={0.2} />
     </View>
   );
 };
