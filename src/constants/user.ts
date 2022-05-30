f2d97554061bec236f656051464a5be25b33ae4d
jack-pearson <qize953463876@gmail.com>
2022-05-30 15:50:42 +0800

    更改类型

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -2,9 +2,14 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-27 15:30:38
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-27 15:30:43
+ * @LastEditTime: 2022-05-30 15:47:39
  * @FilePath: \GOSS-APP\src\constants\user.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export const SET_USER = "SET_USER";
 export const REMOVE_USER = "REMOVE_USER";
+
+export enum USER {
+  SET_USER = "SET_USER",
+  REMOVE_USER = "REMOVE_USER",
+}
--- src/reducer/user.ts
+++ src/reducer/user.ts
@@ -2,22 +2,22 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-27 15:21:35
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-30 15:03:12
+ * @LastEditTime: 2022-05-30 15:47:56
  * @FilePath: \GOSS-APP\src\redurce\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { SET_USER, REMOVE_USER } from "@/constants/user";
-import { IAction } from "./type";
+import { USER } from "@/constants/user";
+import { IActionUser } from "./type";
 
 let INITIAL_STATE = {};
 
-export default function user(state = INITIAL_STATE, action: IAction) {
+export default function user(state = INITIAL_STATE, action: IActionUser) {
   switch (action.type) {
-    case SET_USER:
+    case USER.SET_USER:
       return {
         ...action.user,
       };
-    case REMOVE_USER:
+    case USER.REMOVE_USER:
       return {};
     default:
       return state;
