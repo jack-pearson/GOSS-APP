2345694478dc1b193b25ee231813fd6742454b7b
jack-pearson <qize953463876@gmail.com>
2022-05-31 15:34:14 +0800

    修改类型

--- src/reducer/type.ts
+++ src/reducer/type.ts
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-30 14:58:17
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-30 15:49:33
+ * @LastEditTime: 2022-05-31 14:50:43
  * @FilePath: \GOSS-APP\src\reducer\type.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,4 +13,8 @@ export interface IAction {
   [key: string]: any;
 }
 
-export type IActionUser = IAction & USER;
+export type IUser = typeof USER;
+
+export type IActionUser = IAction & {
+  type: keyof IUser;
+};
