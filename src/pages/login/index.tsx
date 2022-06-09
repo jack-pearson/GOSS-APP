e2fc278ba7c0c946fb6e6c4f2397d7b5512f4eda
jack-pearson <qize953463876@gmail.com>
2022-06-09 19:13:25 +0800

    增加登陆按钮的disabled

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-27 14:27:11
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-06-09 19:09:36
+ * @LastEditTime: 2022-06-09 19:12:42
  * @FilePath: \GOSS-APP\src\pages\login.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -78,7 +78,7 @@ const LoginPage = ({ user, setUser, removeUser }) => {
         />
       </View>
       <View>
-        <Button type='solid' title='登陆' onPress={login}></Button>
+        <Button type='solid' title='登陆' onPress={login} disabled={userInput.password && userInput.account ? false : true}></Button>
       </View>
     </SafeAreaView>
   );
