f6e9ba9f5558b9567340aecdb7832a6e34dc0e89
jack-pearson <qize953463876@gmail.com>
2022-06-14 16:42:34 +0800

    dev 文件修改 删除无用代码

--- src/pages/tabs/home.tsx
+++ src/pages/tabs/home.tsx
@@ -2,13 +2,13 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-30 21:55:01
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-06-09 14:35:05
+ * @LastEditTime: 2022-06-14 15:00:20
  * @FilePath: \GOSS-APP\src\pages\tabs\home.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import FocusAwareStatusBar from "@/components/focusAwareStatusBar";
 import React from "react";
-import { FlatList, StyleSheet, Text, View, StatusBar } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { connect } from "react-redux";
 
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-27 14:27:35
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-06-07 15:42:03
+ * @LastEditTime: 2022-06-14 14:50:07
  * @FilePath: \GOSS-APP\src\router\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,8 +15,6 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Pages from "./importPage";
 import { SET_USER } from "@/constants/user";
 import { CustomIcons } from "@/components";
-import { StatusBar } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
 const Router = () => {
