9ffe363bd63992aaa467502b3eeec4d50a32a009
jack-pearson <qize953463876@gmail.com>
2022-06-16 14:48:44 +0800

    app 打包测试

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-27 14:27:11
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-06-09 19:34:50
+ * @LastEditTime: 2022-06-16 14:48:29
  * @FilePath: \GOSS-APP\src\pages\login.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
