bb3df2f5b879b817e8b3fcac437acc0cd3825042
jack-pearson <qize953463876@gmail.com>
2022-06-16 18:11:19 +0800

    增加启动屏测试

--- App.tsx
+++ App.tsx
@@ -2,18 +2,22 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-26 13:47:00
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-06-08 15:28:41
+ * @LastEditTime: 2022-06-16 18:05:04
  * @FilePath: \GOSS-APP\App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
+import SplashScreen from "react-native-splash-screen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Router from "@/router";
 import Store from "@/store";
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={Store}>
       <SafeAreaProvider>
--- android/app/src/main/java/com/awesometsproject/MainActivity.java
+++ android/app/src/main/java/com/awesometsproject/MainActivity.java
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-26 13:47:00
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-26 16:55:49
+ * @LastEditTime: 2022-06-16 18:10:07
  * @FilePath: \GOSS-APP\android\app\src\main\java\com\awesometsproject\MainActivity.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,11 +10,13 @@ package com.awesometsproject;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
+ import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, true);
     super.onCreate(null);
   }
   /**
