15f5d99e2645a29c192411b2da977c7e20a39667
jack-pearson <qize953463876@gmail.com>
2022-06-16 23:00:17 +0800

    android 启动屏幕完成.

--- android/app/src/main/java/com/awesometsproject/MainActivity.java
+++ android/app/src/main/java/com/awesometsproject/MainActivity.java
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-26 13:47:00
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-06-16 18:10:07
+ * @LastEditTime: 2022-06-16 22:52:02
  * @FilePath: \GOSS-APP\android\app\src\main\java\com\awesometsproject\MainActivity.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,7 @@ package com.awesometsproject;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
- import org.devio.rn.splashscreen.SplashScreen;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -2,7 +2,7 @@
  * @Author: jack-pearson qize953463876@gmail.com
  * @Date: 2022-05-31 14:33:43
  * @LastEditors: jack-pearson qize953463876@gmail.com
- * @LastEditTime: 2022-05-31 14:37:26
+ * @LastEditTime: 2022-06-16 22:56:40
  * @FilePath: \GOSS-APP\src\components\icon\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,5 +11,6 @@ import React from "react";
 import { Text } from "react-native";
 
 export const CustomIcons = (name: keyof IIconConfig, color, size) => {
+  console.log(name);
   return <Text style={{ fontFamily: "iconfont", color, fontSize: size }}>{getIconCode(name)}</Text>;
 };
